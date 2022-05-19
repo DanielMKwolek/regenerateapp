25682d469bf0a0a4a7957583887888a46248a7d6
Danny Kwolek <Daniel.m.kwolek@gmail.com>
2022-05-19 13:33:30 -0700

    continued framework of applePhone component

--- src/Components/ApplePhone/ApplePhone.js
+++ src/Components/ApplePhone/ApplePhone.js
@@ -1,14 +1,23 @@
 const ApplePhone = (props) => {
     return(
         <div className="applePhone">
-            <header className="bls">
-                <div className="appleBackButtonDiv la">
-                    <div className="appleBackChevron bla"> </div>
-                    <p onClick={() => {}}className="fss">Back</p>
+            <header className="appleTitleBar">
+                <div className="appleBackButtonDiv da">
+                    <div className="appleBackChevron bda"> </div>
+                    <p onClick={() => {}}className="fsam">Back</p>
                 </div>
-                <p className="la fsm">Regenerate</p>
+                <p className="appleTitle da fsal">Regenerate</p>
                 <div />
             </header>
+            <div className="appleBody">
+                <form className="appleForm bda">
+                    <input className="da fsas" type="text" placeholder="Name"/>
+                    <input className="da fsas" type="text" placeholder="Username"/>
+                    <input className="da fsas" type="text" placeholder="Email"/>
+                    <input className="da fsas" type="password" placeholder="Password"/>
+                    <input className="da fsas" type="password" placeholder="Repeat Password"/>
+                </form>
+            </div> 
         </div>
     );
 }
