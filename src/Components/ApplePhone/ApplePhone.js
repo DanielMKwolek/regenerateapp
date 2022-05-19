99d61181182018013a60025951fa9820a2b5e9f4
Danny Kwolek <Daniel.m.kwolek@gmail.com>
2022-05-19 10:17:55 -0700

    Used png of chevron image to mask over bg color to make a chevron with matching color scheme to UI

--- src/Components/ApplePhone/ApplePhone.js
+++ src/Components/ApplePhone/ApplePhone.js
@@ -1,10 +1,9 @@
-import chevron from '../../assets/images/rect10.png'
 const ApplePhone = (props) => {
     return(
         <div className="applePhone">
             <header className="bls">
-                <div className="backButtonDiv la">
-                    <img src={chevron} />
+                <div className="appleBackButtonDiv la">
+                    <div className="appleBackChevron bla"> </div>
                     <p onClick={() => {}}className="fss">Back</p>
                 </div>
                 <p className="la fsm">Regenerate</p>
